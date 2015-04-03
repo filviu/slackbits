712b24ab05f576480a7708ef900c49d5115dd433
silviuvulcan <silviuvulcan@users.noreply.github.com>
2015-04-03 15:42:25 +0300

    fixed for error no entry data.tar.lzma in archive

--- slackbuilds/google-chrome/download-chrome.sh
+++ slackbuilds/google-chrome/download-chrome.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
